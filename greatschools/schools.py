8050468164995f0e0bc40222e6dd2f2f355fadb0
JackKirbyCook82 <jackcookchee@yahoo.com>
2021-12-15 17:31:18 -0600

    Updated

--- greatschools/schools.py
+++ greatschools/schools.py
@@ -13,7 +13,6 @@ import logging
 import traceback
 import regex as re
 from abc import ABC
-from fractions import Fraction
 
 MAIN_DIR = os.path.dirname(os.path.realpath(__file__))
 MOD_DIR = os.path.abspath(os.path.join(MAIN_DIR, os.pardir))
@@ -43,12 +42,12 @@ from webscraping.webquerys import WebQuery, WebDataset
 from webscraping.webqueues import WebScheduler, WebQueueable
 from webscraping.webdownloaders import WebDownloader, CacheMixin
 from webscraping.webdata import WebClickable, WebText, WebTexts, WebCaptcha
-from webscraping.webactions import WebScroll
+from webscraping.webactions import WebScroll, WebMoveToClick
 from webscraping.webvariables import Address, Price
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["Greatschools_Schools_WebDelayer", "Greatschools_Schools_WebDownloader", "Greatschools_Schools_WebReader", "Greatschools_Schools_WebDownloader", "Greatschools_Schools_WebScheduler"]
+__all__ = ["Greatschools_Schools_WebDelayer", "Greatschools_Schools_WebBrowser", "Greatschools_Schools_WebReader", "Greatschools_Schools_WebDownloader", "Greatschools_Schools_WebScheduler"]
 __copyright__ = "Copyright 2021, Jack Kirby Cook"
 __license__ = ""
 
@@ -95,37 +94,35 @@ grade_pattern = "(?<=Grades |Grade )[0-9A-Z-]+"
 identity_parser = lambda x: str(re.findall(identity_pattern, x)[0])
 address_parser = lambda x: str(Address.fromsearch(x))
 price_parser = lambda x: str(Price.fromsearch(x))
-key_parser = lambda x: str(x).strip()
+str_parser = lambda x: str(x).strip()
 type_parser = lambda x: str(re.findall(type_pattern, x)[0]).lower().title()
 grade_parser = lambda x: str(re.findall(grade_pattern, x)[0]).replace("-", "|")
-percent_parser = lambda x: int(re.findall("\d+(?=\%$)", x)[0])
-score_parser = lambda x: int(str(x).strip().split("/")[0]) if bool(str(x).strip()) else None
-fraction_parser = lambda x: float(Fraction(str(x).replace(":", "/")))
-char_parsers = {"%": percent_parser, ":": fraction_parser, "$": price_parser}
-char_parser = lambda x: char_parsers[re.findall("\%\:\$", x)[0]](x) if re.findall("\%\:\$", x)[0] else str(x)
 
 
 class Greatschools_Captcha(WebCaptcha, loader=captcha_webloader, optional=True): pass
 class Greatschools_Address(WebText, loader=address_webloader, parsers={"data": address_parser}): pass
 class Greatschools_Filtration(WebTexts, loader=filtration_webloader): pass
-class Greatschools_Name(WebText, loader=name_webloader, parsers={"data": key_parser}): pass
+class Greatschools_Name(WebText, loader=name_webloader, parsers={"data": str_parser}): pass
 class Greatschools_Type(WebText, loader=details_webloader, parsers={"data": type_parser}): pass
 class Greatschools_Grades(WebText, loader=details_webloader, parsers={"data": grade_parser}): pass
-class Greatschools_ScoreKeys(WebTexts, loader=score_keys_webloader, parsers={"data": key_parser}, optional=True): pass
-class Greatschools_ScoreValues(WebTexts, loader=score_values_webloader, parsers={"data": score_parser}, optional=True): pass
-class Greatschools_TestKeys(WebTexts, loader=test_keys_webloader, parsers={"data": key_parser}, optional=True): pass
-class Greatschools_TestValues(WebTexts, loader=test_values_webloader, parsers={"data": percent_parser}, optional=True): pass
-class Greatschools_DemographicKeys(WebTexts, loader=demographic_keys_webloader, parsers={"data": key_parser}, optional=True): pass
-class Greatschools_DemographicValues(WebTexts, loader=demographic_values_webloader, parsers={"data": percent_parser}, optional=True): pass
-class Greatschools_TeacherKeys(WebTexts, loader=teacher_keys_webloader, parsers={"data": key_parser}, optional=True): pass
-class Greatschools_TeacherValues(WebTexts, loader=teacher_values_webloader, parsers={"data": char_parser}, optional=True): pass
-class Greatschools_TeacherMore(WebClickable, loader=teacher_more_xpath, optional=True): pass
+class Greatschools_ScoreKeys(WebTexts, loader=score_keys_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_ScoreValues(WebTexts, loader=score_values_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_TestKeys(WebTexts, loader=test_keys_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_TestValues(WebTexts, loader=test_values_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_DemographicKeys(WebTexts, loader=demographic_keys_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_DemographicValues(WebTexts, loader=demographic_values_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_TeacherKeys(WebTexts, loader=teacher_keys_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_TeacherValues(WebTexts, loader=teacher_values_webloader, parsers={"data": str_parser}, optional=True): pass
+class Greatschools_TeacherMore(WebClickable, loader=teacher_more_webloader, optional=True): pass
 
 
 class Greatschools_Scroll(WebScroll, wait=3): pass
+class Greatschools_TeacherMore_MoveToClick(WebMoveToClick, on=Greatschools_TeacherMore): pass
+
+
 class Greatschools_Schools_WebDelayer(WebDelayer): pass
-class Greatschools_Schools_WebReader(WebReader, retrys=Retrys(retries=3, backoff=0.3, httpcodes=(500, 502, 504)), headers=Headers(UserAgents.load(USERAGENTS_FILE, limit=100))): pass
 class Greatschools_Schools_WebBrowser(WebBrowser, files={"chrome": DRIVER_EXE}, options={"headless": False, "images": True, "incognito": False}): pass
+class Greatschools_Schools_WebReader(WebReader, retrys=Retrys(retries=3, backoff=0.3, httpcodes=(500, 502, 504)), headers=Headers(UserAgents.load(USERAGENTS_FILE, limit=100))): pass
 
 
 class Greatschools_Schools_WebURL(WebURL, protocol="https", domain="www.greatschools.org"):
@@ -135,7 +132,7 @@ class Greatschools_Schools_WebURL(WebURL, protocol="https", domain="www.greatsch
 
 
 class Greatschools_Schools_WebQuery(WebQuery, WebQueueable, fields=["GID"]): pass
-class Greatschools_Schools_WebDataset(WebDataset, fields=["school", "scores", "testing", "demographics"]): pass
+class Greatschools_Schools_WebDataset(WebDataset, fields=["school", "scores", "testing", "demographics", "teachers"]): pass
 
 
 class Greatschools_Schools_WebScheduler(WebScheduler, fields=["GID"]):
@@ -157,7 +154,7 @@ class Greatschools_Schools_WebScheduler(WebScheduler, fields=["GID"]):
         return list(dataframe["GID"].to_numpy())
 
     @staticmethod
-    def execute(querys, *args, **kwargs): return [Greatschools_Schools_WebQuery(query) for query in querys]
+    def execute(querys, *args, **kwargs): return [Greatschools_Schools_WebQuery(query, name="GreatSchoolsQuery") for query in querys]
 
 
 class Greatschools_Schools_WebContents(WebPageContents):
@@ -183,11 +180,11 @@ class Greatschools_Schools_WebPage(WebContentPage, ABC, contents=Greatschools_Sc
     def query(self): return {"GID": str(identity_parser(self.url))}
 
     def setup(self, *args, **kwargs):
-        try:
-            Greatschools_Scroll(self.driver)(*args, commands={"pagedown": 12}, **kwargs)
-            Greatschools_TeacherMore(self.driver)(*args, **kwargs)
-        except AttributeError:
-            pass
+        if hasattr(self, "driver"):
+            Greatschools_Scroll(self.driver)(*args, commands={"pagedown": 20}, **kwargs)
+            more = Greatschools_TeacherMore_MoveToClick(self.driver)
+            if bool(bool(more)):
+                more(*args, **kwargs)
 
     def execute(self, *args, **kwargs):
         query = self.query()
@@ -216,7 +213,7 @@ class Greatschools_Schools_WebPage(WebContentPage, ABC, contents=Greatschools_Sc
         testing = {key.data(): value.data() for key, value in items}
         return {**self.query(), **testing}
 
-    @webpage_bypass(condition=lambda self: not bool(self[Greatschools_Schools_WebContents.DEMOGRAPHICS_KEYS]), value=None)
+    @webpage_bypass(condition=lambda self: not bool(self[Greatschools_Schools_WebContents.DEMOGRAPHIC_KEYS]), value=None)
     def demographics(self):
         items = zip(iter(self[Greatschools_Schools_WebContents.DEMOGRAPHIC_KEYS]), iter(self[Greatschools_Schools_WebContents.DEMOGRAPHIC_VALUES]))
         demographics = {key.data(): value.data() for key, value in items}
@@ -224,10 +221,8 @@ class Greatschools_Schools_WebPage(WebContentPage, ABC, contents=Greatschools_Sc
 
     @webpage_bypass(condition=lambda self: not bool(self[Greatschools_Schools_WebContents.TEACHER_KEYS]), value=None)
     def teachers(self):
-        fields = {"Students per teacher": "STR", "Students per counselor": "SCR", "Average teacher salary": "Salary",
-                  "% of teachers with 3 or more years experience ": "EXP", "% of full time teachers who are certified": "CERT"}
         items = zip(iter(self[Greatschools_Schools_WebContents.TEACHER_KEYS]), iter(self[Greatschools_Schools_WebContents.TEACHER_VALUES]))
-        demographics = {key.data(): value.data() for key, value in items if key in fields.keys()}
+        demographics = {key.data(): value.data() for key, value in items}
         return {**self.query(), **demographics}
 
 
@@ -241,6 +236,8 @@ class Greatschools_Schools_WebDownloader(CacheMixin, WebVPNProcess, WebDownloade
                 for query in queue:
                     if not bool(self.vpn):
                         self.wait()
+                    if not bool(driver):
+                        driver.reset()
                     with query:
                         url = self.url(**query.todict())
                         url = Greatschools_Schools_WebURL.fromstr(str(url))
@@ -249,9 +246,9 @@ class Greatschools_Schools_WebDownloader(CacheMixin, WebVPNProcess, WebDownloade
                             page.setup(*args, **kwargs)
                             for fields, dataset, data in page(*args, **kwargs):
                                 yield Greatschools_Schools_WebQuery(fields), Greatschools_Schools_WebDataset({dataset: data})
-                        except (RefusalError, CaptchaError) as error:
+                        except (RefusalError, CaptchaError):
+                            driver.trip()
                             self.trip()
-                            raise error
                         except BadRequestError:
                             pass
 
@@ -269,16 +266,14 @@ def main(*args, **kwargs):
     delayer = Greatschools_Schools_WebDelayer(name="GreatSchoolsDelayer", method="random", wait=(30, 60))
     reader = Greatschools_Schools_WebReader(name="GreatSchoolsReader", wait=10)
     browser = Greatschools_Schools_WebBrowser(name="GreatSchoolsBrowser", browser="chrome", loadtime=50, wait=10)
-    scheduler = Greatschools_Schools_WebScheduler(name="GreatSchoolsScheduler", file=REPORT_FILE, size=100)
+    scheduler = Greatschools_Schools_WebScheduler(name="GreatSchoolsScheduler", file=REPORT_FILE, size=10)
     downloader = Greatschools_Schools_WebDownloader(name="GreatSchools", repository=REPOSITORY_DIR)
     vpn = Nord_WebVPN(name="NordVPN", file=NORDVPN_EXE, server="United States", loadtime=10, wait=10)
     vpn += downloader
-    for queue in iter(scheduler)(*args, **kwargs):
-        downloader(**dict(browser=browser, reader=reader, queue=queue, delayer=delayer))
-        vpn.start()
-        vpn.join()
-        if bool(downloader.error):
-            break
+    queue = scheduler(*args, **kwargs)
+    downloader(**dict(browser=browser, reader=reader, queue=queue, delayer=delayer))
+    vpn.start()
+    vpn.join()
     for query, results in downloader.results:
         LOGGER.info(str(query))
         LOGGER.info(str(results))
